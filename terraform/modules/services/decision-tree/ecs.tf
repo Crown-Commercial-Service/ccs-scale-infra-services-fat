68354fcf60d4514b6747cfe5f70367281020c0ed
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-06-01 00:09:36 +0100

    SFAT-153 - move DecisionTreeDB to DB subnet

--- terraform/modules/services/decision-tree/ecs.tf
+++ terraform/modules/services/decision-tree/ecs.tf
@@ -76,71 +76,45 @@ resource "aws_ecs_task_definition" "decision_tree" {
   container_definitions = <<DEFINITION
     [
     {
-        "name": "SCALE-EU2-${upper(var.environment)}-APP-ECS_TaskDef_DecisionTree",
-        "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/decision-tree-service:d3e5bfb-candidate",
+      "name": "SCALE-EU2-${upper(var.environment)}-APP-ECS_TaskDef_DecisionTree",
+      "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/decision-tree-service:d3e5bfb-candidate",
+      "requires_compatibilities": "FARGATE",
+      "cpu": 256,
+      "memory": 512,
+      "essential": true,
+      "networkMode": "awsvpc",
+      "portMappings": [
+          {
+          "containerPort": 9000,
+          "hostPort": 9000
+          }
+      ],
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+            "awslogs-group": "${aws_cloudwatch_log_group.fargate_scale.name}",
+            "awslogs-region": "eu-west-2",
+            "awslogs-stream-prefix": "fargate-decision-tree"
+        }
+      }
+    },
+      {
+        "name": "SCALE-EU2-${upper(var.environment)}-APP-ECS_TaskDef_LookupService",
+        "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/lookup-service:5b5b654-candidate",
         "requires_compatibilities": "FARGATE",
         "cpu": 256,
         "memory": 512,
         "essential": true,
         "networkMode": "awsvpc",
-        "portMappings": [
-            {
-            "containerPort": 9000,
-            "hostPort": 9000
-            }
-        ],
         "logConfiguration": {
           "logDriver": "awslogs",
           "options": {
               "awslogs-group": "${aws_cloudwatch_log_group.fargate_scale.name}",
               "awslogs-region": "eu-west-2",
-              "awslogs-stream-prefix": "fargate-decision-tree"
+              "awslogs-stream-prefix": "fargate-lookup-service"
           }
         }
-      },
-      {
-          "name": "SCALE-EU2-${upper(var.environment)}-APP-ECS_TaskDef_LookupService",
-          "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/lookup-service:5b5b654-candidate",
-          "requires_compatibilities": "FARGATE",
-          "cpu": 256,
-          "memory": 512,
-          "essential": true,
-          "networkMode": "awsvpc",
-          "logConfiguration": {
-            "logDriver": "awslogs",
-            "options": {
-                "awslogs-group": "${aws_cloudwatch_log_group.fargate_scale.name}",
-                "awslogs-region": "eu-west-2",
-                "awslogs-stream-prefix": "fargate-lookup-service"
-            }
-          }
-      },
-      {
-          "name": "SCALE-EU2-${upper(var.environment)}-APP-ECS_TaskDef_DecisionTreeDB",
-          "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/decision-tree-db:2a80cc3-candidate",
-          "requires_compatibilities": "FARGATE",
-          "cpu": 512,
-          "memory": 1024,
-          "essential": true,
-          "networkMode": "awsvpc",
-          "portMappings": [
-              {
-              "containerPort": 7687,
-              "hostPort": 7687
-              }
-          ],
-          "logConfiguration": {
-            "logDriver": "awslogs",
-            "options": {
-                "awslogs-group": "${aws_cloudwatch_log_group.fargate_scale.name}",
-                "awslogs-region": "eu-west-2",
-                "awslogs-stream-prefix": "fargate-neo4j"
-            }
-          },
-          "environment": [
-            { "name": "NEO4J_AUTH", "value": "neo4j/sbx_graph" }
-          ]
-          }
+      }
     ]
 DEFINITION
 
@@ -154,5 +128,78 @@ DEFINITION
 
 resource "aws_cloudwatch_log_group" "fargate_scale" {
   name_prefix       = "/fargate/service/scale/decision-tree"
+  retention_in_days = 7
+}
+
+resource "aws_ecs_service" "decision_tree_db" {
+  name             = "SCALE-EU2-${upper(var.environment)}-APP-ECS_Service_DecisionTreeDB"
+  cluster          = var.ecs_cluster_id
+  task_definition  = aws_ecs_task_definition.decision_tree.arn
+  launch_type      = "FARGATE"
+  platform_version = "LATEST"
+  desired_count    = 1
+
+  network_configuration {
+    security_groups  = [var.ecs_security_group_id]
+    subnets          = var.private_db_subnet_ids
+    assign_public_ip = false # Replace NAT GW and disable this by replacement AWS PrivateLink
+  }
+
+  #load_balancer {
+  #  target_group_arn = aws_lb_target_group.target_group_9000.arn
+  #  container_name   = "SCALE-EU2-${upper(var.environment)}-APP-ECS_TaskDef_DecisionTree"
+  #  container_port   = 9000
+  #}
+}
+
+resource "aws_ecs_task_definition" "decision_tree_db" {
+  family                   = "decision-tree"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  execution_role_arn       = var.ecs_task_execution_arn
+
+  container_definitions = <<DEFINITION
+    [
+      {
+        "name": "SCALE-EU2-${upper(var.environment)}-APP-ECS_TaskDef_DecisionTreeDB",
+        "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/decision-tree-db:2a80cc3-candidate",
+        "requires_compatibilities": "FARGATE",
+        "cpu": 512,
+        "memory": 1024,
+        "essential": true,
+        "networkMode": "awsvpc",
+        "portMappings": [
+            {
+            "containerPort": 7687,
+            "hostPort": 7687
+            }
+        ],
+        "logConfiguration": {
+          "logDriver": "awslogs",
+          "options": {
+              "awslogs-group": "${aws_cloudwatch_log_group.fargate_scale_db.name}",
+              "awslogs-region": "eu-west-2",
+              "awslogs-stream-prefix": "fargate-neo4j"
+          }
+        },
+        "environment": [
+          { "name": "NEO4J_AUTH", "value": "neo4j/sbx_graph" }
+        ]
+      }
+    ]
+DEFINITION
+
+  tags = {
+    Project     = module.globals.project_name
+    Environment = upper(var.environment)
+    Cost_Code   = module.globals.project_cost_code
+    AppType     = "ECS"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "fargate_scale_db" {
+  name_prefix       = "/fargate/service/scale/decision-tree-db"
   retention_in_days = 7
 }
