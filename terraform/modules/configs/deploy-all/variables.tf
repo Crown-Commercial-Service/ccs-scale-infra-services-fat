9cc97566a53ec39b87f4f88a6bdbdd69d847841f
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-06-17 16:16:15 +0100

    SFAT-228 - bumped BuyerUI to 174fbc0-candidate

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type = string
-  default = "766bf92-candidate"
+  default = "174fbc0-candidate"
 }
 
 variable "decision_tree_cpu" {
