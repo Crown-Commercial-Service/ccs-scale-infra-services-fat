412f95d4730dd10382b31d2078f92df4490d4ddf
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-07-08 10:58:48 +0100

    SFAT-262 - buyerUI - c30546c-candidate (#43)

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "f9592b0-candidate"
+  default = "c30546c-candidate"
 }
 
 variable "decision_tree_service_cpu" {
