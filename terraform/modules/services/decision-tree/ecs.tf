dbe8c178ad092412ea0d4cad2d30f8c7f77a937e
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2020-07-15 11:46:31 +0100

    GM+DT service plus DB version increase (#52)

--- terraform/modules/services/decision-tree/ecs.tf
+++ terraform/modules/services/decision-tree/ecs.tf
@@ -85,7 +85,7 @@ resource "aws_ecs_task_definition" "decision_tree" {
     [
     {
         "name": "SCALE-EU2-${upper(var.environment)}-APP-ECS_TaskDef_DecisionTreeSvc",
-        "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/decision-tree-service:c3de761-candidate",
+        "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/decision-tree-service:fa32e3f-candidate",
         "requires_compatibilities": "FARGATE",
         "cpu": ${var.decision_tree_service_cpu},
         "memory": ${var.decision_tree_service_memory},
