c8eb8132222e36d5441882c03f7c42727ea2f632
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-07-15 14:55:42 +0100

    Feature/sfat 192 pre pro envs (#53)
    * SFAT-192 - created PPD and PRD environments
    
    * SFAT-192 - waiting on web cms URL confirmation
    
    * SFAT-192 - updated webcms_root_url values for ppd and prd

---
+++ terraform/environments/ppd/main.tf
@@ -0,0 +1,42 @@
+#########################################################
+# Environment: PPD (Pre Production)
+#
+# Deploy SCALE resources
+#########################################################
+terraform {
+  backend "s3" {
+    bucket         = "scale-terraform-state"
+    key            = "ccs-scale-infra-services-fat-ppd"
+    region         = "eu-west-2"
+    dynamodb_table = "scale_terraform_state_lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+locals {
+  environment = "PPD"
+}
+
+data "aws_ssm_parameter" "aws_account_id" {
+  name = "account-id-${lower(local.environment)}"
+}
+
+module "deploy" {
+  source                       = "../../modules/configs/deploy-all"
+  aws_account_id               = data.aws_ssm_parameter.aws_account_id.value
+  environment                  = local.environment
+  decision_tree_service_cpu    = 1024
+  decision_tree_service_memory = 2048
+  decision_tree_db_cpu         = 1024
+  decision_tree_db_memory      = 2048
+  guided_match_cpu             = 1024
+  guided_match_memory          = 2048
+  buyer_ui_cpu                 = 1024
+  buyer_ui_memory              = 2048
+  webcms_root_url              = "https://webuat-cms.crowncommercial.gov.uk/" # Pre-prod
+}
