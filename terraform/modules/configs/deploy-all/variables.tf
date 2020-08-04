5c33f5538d1b10b8876efdcd45b104797c36b776
Favian Poputa <favian.poputa@cognizant.com>
2020-08-04 10:38:18 +0300

    deploy new changes

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "f64655f-candidate"
+  default = "8b68190-candidate"
 }
 
 variable "decision_tree_service_cpu" {
