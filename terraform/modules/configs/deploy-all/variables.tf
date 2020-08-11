ca4dbcde64314e6bf72904fea95c078267ae9852
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-08-11 10:23:43 +0100

    SFAT-311 - updated buyerUI to 3efa377-candidate

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "5289bfd-candidate"
+  default = "3efa377-candidate"
 }
 
 variable "decision_tree_service_cpu" {
