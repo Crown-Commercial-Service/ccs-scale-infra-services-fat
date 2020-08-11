94d534bbfdbb84c56f083d66acb73cbcf391a9e5
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-08-11 14:36:03 +0100

    SFAT-311 - added /guided-match/health-check endpoint (#88)

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "3efa377-candidate"
+  default = "453d0cd-candidate"
 }
 
 variable "decision_tree_service_cpu" {
