95caf8e68500ae39b23786c32a14b45d558b1244
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-08-13 10:26:16 +0100

    SFC-311 - BuyerUi - b814179-candidate (#90)

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "453d0cd-candidate"
+  default = "b814179-candidate"
 }
 
 variable "decision_tree_service_cpu" {
