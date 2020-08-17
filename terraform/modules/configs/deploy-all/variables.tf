b6ffcf7c5c559d6b92b52b3ed0ffc0583ba5705b
tommyb82 <thomasbunting@gmail.com>
2020-08-18 00:56:44 +0100

    EMERGENCY FIX - revert buyer-ui version to 4b06597-candidate

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "28ba7fb-candidate"
+  default = "4b06597-candidate"
 }
 
 variable "decision_tree_service_cpu" {
