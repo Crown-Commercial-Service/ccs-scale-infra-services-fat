8612ab20feef5babeb3aa583ac29f296ace614c1
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-09-03 13:32:32 +0100

    SFC-1059,1048,1051 - Decision Tree DB legal content changes

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "74edfc7-candidate"
+  default = "7563254-candidate"
 }
 
 variable "decision_tree_service_cpu" {
