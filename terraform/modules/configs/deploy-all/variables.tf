432efb37d0d8afb5f64276e2ce2c2a6db0cebb3e
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2020-09-08 16:40:30 +0100

    Modify housing and estates question text (#138)

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "6aa0d2b-candidate"
+  default = "5152f86-candidate"
 }
 
 variable "decision_tree_service_cpu" {
