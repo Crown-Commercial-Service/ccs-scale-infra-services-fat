d73fe2c7b606689fabb4dcf139613cfae24e9e40
tommyb82 <thomasbunting@gmail.com>
2020-09-10 11:21:23 +0100

    Bump DT DB version to 4ad666a-candidate

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "5152f86-candidate"
+  default = "4ad666a-candidate"
 }
 
 variable "decision_tree_service_cpu" {
