4833dc4a07f0e7d9eea56696221385aefe0f795d
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2020-09-14 13:24:13 +0100

    Bump FaT Buyer UI to 6a36677-candidate-r1.1 (dockerfile) (#140)

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "79cd995-candidate"
+  default = "6a36677-candidate-r1.1"
 }
 
 variable "ecr_image_id_guided_match" {
