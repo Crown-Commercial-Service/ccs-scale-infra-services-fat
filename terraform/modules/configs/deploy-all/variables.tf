d840aef20824aa575fc646d40cdd430dd6831774
tommyb82 <thomasbunting@gmail.com>
2020-09-15 17:34:56 +0100

    Bump UI version to fa97e47-candidate

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "79cd995-candidate"
+  default = "fa97e47-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
