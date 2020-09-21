d5de610c72eed0c563b86e08f41bdc60c6281eb3
tommyb82 <thomasbunting@gmail.com>
2020-09-21 16:39:53 +0100

    DT DB version bump to 745b9a4-candidate-release-1-1

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "3f89cde-candidate-release-1-1"
+  default = "745b9a4-candidate-release-1-1"
 }
 
 variable "decision_tree_service_cpu" {
