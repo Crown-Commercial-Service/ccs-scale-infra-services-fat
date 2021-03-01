9952204392c50ec5a9f89d94fa4f2be715d2cfb6
jalvares-crown <70591203+jalvares-crown@users.noreply.github.com>
2021-03-01 14:55:14 +0000

    updated decision tree db candidate

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "2cb9369-candidate"
+  default = "7f6327d-candidate"
 }
 
 variable "decision_tree_service_cpu" {
