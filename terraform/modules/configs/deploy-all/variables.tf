480d6bee8618ec38f718741e64e3375afc06f755
jalvares-crown <70591203+jalvares-crown@users.noreply.github.com>
2021-03-02 11:44:21 +0000

    Updated UI candidate

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "964d6a9-candidate"
+  default = "c132cc6-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
