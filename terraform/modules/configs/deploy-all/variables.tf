2de238cfec941c469b9142859fd398537e688c8c
jalvares-crown <70591203+jalvares-crown@users.noreply.github.com>
2021-04-27 10:39:40 +0100

    UI change (#174)

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "78fc8bc-candidate"
+  default = "a090457-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
