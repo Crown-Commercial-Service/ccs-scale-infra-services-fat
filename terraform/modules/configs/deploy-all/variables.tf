c72c4291ecbd3713b118100dfbe2ea3537412efa
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2021-10-07 12:07:02 +0100

    Update variables.tf

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "7ba2b20-candidate"
+  default = "11eca46-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
