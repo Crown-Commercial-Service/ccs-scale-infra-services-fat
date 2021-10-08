a8614fd0d8fce65fd9bbb9df5fd3a27a3f044911
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2021-10-08 12:27:15 +0100

    using umask to fix pipeline

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "4a5227f-candidate"
+  default = "302a658-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
