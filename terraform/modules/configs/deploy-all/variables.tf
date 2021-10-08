5114a2865d002b760c66ced4965225ee7a039d23
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2021-10-08 14:03:52 +0100

    creating the dir before volume

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "302a658-candidate"
+  default = "94220af-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
