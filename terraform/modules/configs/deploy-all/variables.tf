36a193902d63138b284acf7dae1b5bd05b3b58fb
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2021-11-30 14:07:41 +0000

    updated landing page static text

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "348ab79-candidate"
+  default = "1cb11a3-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
