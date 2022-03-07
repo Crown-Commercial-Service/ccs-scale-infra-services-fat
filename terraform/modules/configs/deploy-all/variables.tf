c1d6cd1c348f10e653a9350cdbd5bbdf47fa60fa
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2022-03-07 13:46:52 +0000

    updated docker path to include startup/

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "3729221-candidate"
+  default = "7cef20a-candidate"
 }
 
 variable "decision_tree_service_cpu" {
