6aca2748d8d25bca2dca94a70cc2d3aae67e6568
dorota-hawro <83641223+dorota-hawro@users.noreply.github.com>
2022-11-04 15:26:55 +0000

    New GM for Audit Assurance and Occupational Health

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "385f467-candidate"
+  default = "37d4a8a-candidate"
 }
 
 variable "decision_tree_service_cpu" {
