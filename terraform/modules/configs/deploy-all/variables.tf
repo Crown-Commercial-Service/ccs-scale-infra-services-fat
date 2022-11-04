fd6f04e7303863d6bcfa95d3b6abf4d9081173eb
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2022-11-04 17:07:46 +0000

    show lot clearly on result page on user answers

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "0d5a6f6-candidate"
+  default = "5afa8f0-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
