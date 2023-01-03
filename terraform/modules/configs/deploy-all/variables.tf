0786231f395d1160d83ad919604e4bb326b0c7aa
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-01-03 15:06:58 +0000

    added 2 new GM trees
    added outsourced contact centre and apprenticeship GM trees

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "be35b52-candidate"
+  default = "5bcf772-candidate"
 }
 
 variable "decision_tree_service_cpu" {
