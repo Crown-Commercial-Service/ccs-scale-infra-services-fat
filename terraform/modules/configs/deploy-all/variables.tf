379b8a72ef3a976d329f768daad295ad2e193787
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-02-07 11:18:24 +0000

    Turning off maintenance

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "2f286f4-candidate"
+  default = "0aa3e9c-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
