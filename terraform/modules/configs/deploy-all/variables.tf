6a8d69b8a97cfe6c16abaaa4c32f9bdb362fecfd
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-02-22 10:56:15 +0000

    amended office tree

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "c8a0072-candidate"
+  default = "e827677-candidate"
 }
 
 variable "decision_tree_service_cpu" {
