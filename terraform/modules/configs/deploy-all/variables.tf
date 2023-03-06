3bc58fdb1a45453ca451d7470582159a30a6fdba
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-03-06 14:55:52 +0000

    added Marcomms DT

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "7f5727e-candidate"
+  default = "d902c44-candidate"
 }
 
 variable "decision_tree_service_cpu" {
