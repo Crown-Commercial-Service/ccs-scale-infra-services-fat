667fec3f259cd6713b1a494abe8ab97e9eb58810
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-03-09 11:16:40 +0000

    Fixed Staffing DT

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "04c0234-candidate"
+  default = "d30d7be-candidate"
 }
 
 variable "decision_tree_service_cpu" {
