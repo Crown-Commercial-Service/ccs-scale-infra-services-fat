5684dc173622c69ee4d4e821862ebb801559639b
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-03-16 12:26:33 +0000

    added Consultancy DT

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "d3c1a77-candidate"
+  default = "336dde3-candidate"
 }
 
 variable "decision_tree_service_cpu" {
