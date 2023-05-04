0580197c869e56894d4794f4ccc103b3002dcdc6
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-05-04 14:00:29 +0100

    added Physical, CWAS and E Disclosure 2 DT

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "336dde3-candidate"
+  default = "4f9097f-candidate"
 }
 
 variable "decision_tree_service_cpu" {
