520e1f4afc7742c94ef9da432d658246150565d5
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-05-05 14:02:40 +0100

    More user friendly url for Agreement service

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "9cc9e94-candidate"
+  default = "9a15f77-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
