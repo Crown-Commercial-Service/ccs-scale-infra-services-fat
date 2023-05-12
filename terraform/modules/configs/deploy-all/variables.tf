ff73c79c2667c3bd93447b8043bce3cf085be1ab
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-05-12 10:47:23 +0100

    added Payment Solutions 2 DT

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "751b6e4-candidate"
+  default = "f9a0f0b-candidate"
 }
 
 variable "decision_tree_service_cpu" {
