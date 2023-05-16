47c3eca42a54f5504cee959229a2eba8df09f66f
zubairali6 <69308726+zubairali6@users.noreply.github.com>
2023-05-16 15:42:04 +0100

    Disable Debug Mode on PPD

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "e9408af-candidate"
+  default = "331ac9e-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
