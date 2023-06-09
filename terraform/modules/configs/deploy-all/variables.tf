e197553d16fc09ac9e941d8fcb7f9f63edb83d2c
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-06-09 10:37:57 +0100

    agreements are now sorted

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "5684b24-candidate"
+  default = "1fc60f6-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
