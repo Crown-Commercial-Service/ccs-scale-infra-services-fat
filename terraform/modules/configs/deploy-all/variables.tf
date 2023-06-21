257e4055257bf648e34bed09b782b7b62f91b57a
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-06-21 15:23:26 +0100

    removed RM6089

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "028cc4e-candidate"
+  default = "30a27af-candidate"
 }
 
 variable "decision_tree_service_cpu" {
