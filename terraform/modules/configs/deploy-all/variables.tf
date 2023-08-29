c9243151ece3cf84345d0be4982a05f2a8faf61c
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-08-29 10:15:54 +0100

    Update variables.tf

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "c516150-candidate"
+  default = "6d342c7-candidate"
 }
 
 variable "decision_tree_service_cpu" {
