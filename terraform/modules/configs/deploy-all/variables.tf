3a1519bb197b1a0fd4201026307500a05c67085e
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-10-03 10:06:46 +0100

    updated expired endpoint

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "1696e77-candidate"
+  default = "4d24ce6-candidate"
 }
 
 variable "decision_tree_service_cpu" {
