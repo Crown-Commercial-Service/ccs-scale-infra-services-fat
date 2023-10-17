58dce83e8ca0bf3479ec0a21a8d5d1f16604c3fa
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-10-17 12:41:44 +0100

    [Snyk] update camel from 3.0.0 to 3.2.0

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -18,7 +18,7 @@ variable "ecr_image_id_guided_match" {
 
 variable "ecr_image_id_decision_tree" {
   type    = string
-  default = "deb93ad-candidate"
+  default = "d7d31d4-candidate"
 }
 
 variable "ecr_image_id_decision_tree_db" {
