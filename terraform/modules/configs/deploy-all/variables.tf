7ae6eaec95ba74ed660f70035f1d3c6551823a4e
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-04-03 13:28:20 +0100

    removed expired endpoint RM6119, RM6169 and RM6155

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "c6a82ee-candidate"
+  default = "61ffb47-candidate"
 }
 
 variable "decision_tree_service_cpu" {
