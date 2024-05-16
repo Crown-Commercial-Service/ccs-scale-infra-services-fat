4a7de8db2e40beb87d49637c8964c333d2cc92fb
zubairali6 <69308726+zubairali6@users.noreply.github.com>
2024-05-16 11:14:05 +0100

    Remove Bracket Breadcrumb

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "80c25e0-candidate"
+  default = "be7ce4f-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
