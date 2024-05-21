92f7087587320202f69b2b396bcd6ebb8f70faea
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-05-21 10:27:52 +0100

    Taxonomy changes to GM Lite

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "dd1969a-candidate"
+  default = "5eaf432-candidate"
 }
 
 variable "decision_tree_service_cpu" {
