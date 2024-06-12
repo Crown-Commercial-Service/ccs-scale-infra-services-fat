6eecfeb0547c68a9c05d4217a4871662af575145
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-06-12 15:28:20 +0100

    WEB-2379 and WEB-2437

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "5eaf432-candidate"
+  default = "eb9a776-candidate"
 }
 
 variable "decision_tree_service_cpu" {
