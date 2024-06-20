fc865a8ec975b8f8b218f0086c4735380390911a
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-06-20 14:59:03 +0100

    stop dataLayer on DEV and UAT

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "be7ce4f-candidate"
+  default = "b0c8f3b-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
