538dacba8e4bc7cfd1f8cd89880e17dbe09a9ceb
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-08-21 09:16:28 +0100

    added Cloud Compute 2 DT

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "33534db-candidate"
+  default = "02e20ba-candidate"
 }
 
 variable "decision_tree_service_cpu" {
