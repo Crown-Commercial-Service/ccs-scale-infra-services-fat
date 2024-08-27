9fb35ed804d6364ad76911ef091455f277f6f845
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-08-27 16:25:33 +0100

    added Transport Tech Service DT

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "02e20ba-candidate"
+  default = "b27249b-candidate"
 }
 
 variable "decision_tree_service_cpu" {
