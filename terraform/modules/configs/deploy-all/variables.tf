c20feefd50104251936c9bc8ab5776d5e3198af2
zubairali6 <69308726+zubairali6@users.noreply.github.com>
2024-08-27 15:30:32 +0100

    Upgrade to GOVUK Version 5

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "b0c8f3b-candidate"
+  default = "7936f75-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
