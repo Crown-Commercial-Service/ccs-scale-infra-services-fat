c14a34fe2edf207e105c8dbe2a65a1e4f7cf9af4
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-09-27 10:59:36 +0100

    Corrected error

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "d5baa20-candidate"
+  default = "ccba723-candidate"
 }
 
 variable "decision_tree_service_cpu" {
