df883cdfca028cbd73991c3b7825efa076af1806
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-11-22 11:53:08 +0000

    fixed URL for back to search agreement

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "7358900-candidate"
+  default = "2b241d8-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
