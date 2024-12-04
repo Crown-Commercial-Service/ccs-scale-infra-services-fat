1dd72398b0c811f532840e173c28b8cbc15dc97b
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-12-04 15:37:56 +0000

    updated Post and Courier DT

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "ffffea7-candidate"
+  default = "2c58dcd-candidate"
 }
 
 variable "decision_tree_service_cpu" {
