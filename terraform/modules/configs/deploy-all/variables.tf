1c7e58a476f0fcfcda7cebe930b2ad7b144a568d
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-12-16 14:03:30 +0000

    update expired agreements

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "2c58dcd-candidate"
+  default = "be4be0d-candidate"
 }
 
 variable "decision_tree_service_cpu" {
