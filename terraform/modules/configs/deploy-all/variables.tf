ea35b745f6cf193f6e01ba11bc86d94834dfe5ef
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-12-19 11:29:50 +0000

    removing RM6146

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -23,7 +23,7 @@ variable "ecr_image_id_decision_tree" {
 
 variable "ecr_image_id_decision_tree_db" {
   type    = string
-  default = "be4be0d-candidate"
+  default = "cc049c7-candidate"
 }
 
 variable "decision_tree_service_cpu" {
