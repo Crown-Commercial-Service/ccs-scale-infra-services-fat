75b600b7395066d66b65f9ce6ca74500a88ee64b
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2025-04-16 14:51:05 +0100

    added page view for GM and fixed CNZ result page for GA

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "8b5fa43-candidate"
+  default = "2bfb96a-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
