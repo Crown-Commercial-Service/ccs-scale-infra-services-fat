d1d3387cc2ab5b1dd9e0103bd384f3f93d6c0213
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2025-05-29 14:27:01 +0100

    replace all gm to cnz to fix css

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "d4c63af-candidate"
+  default = "e340345-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
