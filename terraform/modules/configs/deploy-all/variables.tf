70d93ac096c29199365830f1b7a8eb8a118b0397
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2025-09-15 12:40:52 +0100

    update back button for CNZ

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "4ae5fa5-candidate"
+  default = "5ae6bae-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
