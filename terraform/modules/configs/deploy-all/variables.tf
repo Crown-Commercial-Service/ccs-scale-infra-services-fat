a7b280113f08fa8481cc8118192e919b099e626f
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2025-09-16 14:19:47 +0100

    removed back link to CW

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "5ae6bae-candidate"
+  default = "1d0047d-candidate"
 }
 
 variable "ecr_image_id_guided_match" {
