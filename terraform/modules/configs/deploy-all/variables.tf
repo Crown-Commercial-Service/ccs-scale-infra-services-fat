70894e4aeecabe447579e60b34b5b10462a484ee
zubairali6 <69308726+zubairali6@users.noreply.github.com>
2025-09-24 10:07:21 +0100

    update footer links

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_fat_buyer_ui" {
   type    = string
-  default = "1d0047d-candidate"
+  default = "f07b370-candidate
 }
 
 variable "ecr_image_id_guided_match" {
